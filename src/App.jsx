dfccd157bf828ca0b7b33ffdd5ad56e1b3098689
ayumu <147569295+ayuayuyu@users.noreply.github.com>
2024-12-01 11:20:55 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-// import Paiza from "./Paiza";\
 import Paiza from "./Paiza";
 
 function App() {
